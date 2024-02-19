81538e32c99404e26ad666f67d691a58a0a1d9db
Avery Harff <averyh@uoregon.edu>
2024-02-18 17:49:08 -0800

    Proposed workflow. will revise if we talk about this more in class. Due 1/19/24 11:59 pm.

---
+++ Harff_A_Proposed workflow.R
@@ -0,0 +1,107 @@
+Proposed Workflow 
+Avery Harff
+
+Title - The Yellow Faced Bombus and how the food supply changes seasonly throughout the state of Oregon.
+1. Literature Review
+  a) Introduce the problem and explain why
+The Yellow Faced Bombus (Bombus vosnesenksii) is a pollinator that can be found up and down the west cost of 
+North America to Bristish Columbia (iNaturalist). During seasonal changes of climate it is questioned what this bee 
+does to surive the colder temperatures. The yellow faced bombus is the most active in the spring, this is due 
+to the new colony of bees that will be arriving in the fall (Daane, 2023). Bombus vosnesenksii are not native to 
+a certain location, but rather the Bombus vosnesenksii can be spotted in subarbs, grasslands, high elevation, and 
+modified landscapes (Nature Serve Explorer). Because the bee has been spotted in numerous places in the west coast,
+the temperature does vary and that effects the bumbus food supply and how the bumbus keep the colony alive and functioning (Strange et al. 2023). 
+Thus, the interest is peeked on how the yellow faced bombus supports the colony in the midst of the changing vegitation along with
+the vastly differing climate temperatues. 
+
+  b) Past work and data available
+James P. Strange addressed the issue of colony size changing with the climate conditions. Strange analized colony size from 2009 - 2019, 
+he found that the colony size in the winter increases because the Bombus vosnesenksii produces more drone/ worker bees compaired to other 
+species of bees. Through his research he was able to help conservationalist with different techniques, such as, utilizing T-tests to compair
+two different speices and the nesting percentage. 
+
+Data available 
+For the colony size I could use the tables that Strange collected.
+For the temperature there is an Oregon climate data website that has data of different averages of weather and parcipitation in the state of Oregon. 
+    Climate data: https://prism.oregonstate.edu/
+To collect data for the yellow faced bombus vegitation througout the different seasons.
+    https://www.oregon.gov/oda/shared/Documents/Publications/PlantConservation/LupinusOreganusProfile.pdf
+    Blue Hydrangea - 
+      https://landscapeplants.oregonstate.edu/plants/hydrangea-macrophylla
+    Eriogonum (june- september, october in higher elevations)       
+      https://extension.oregonstate.edu/gardening/pollinators/eriogonum-umbellatum-sulphur-flower-buckwheat
+    Phacelia (april - july) -
+      https://blogs.oregonstate.edu/gardenecologylab/tag/phacelia/
+    Clarkia (fall and spring) - https://blogs.oregonstate.edu/gardenecologylab/tag/clarkia-amoena/
+    Ericameria (june - november) https://oregonflora.org/taxa/garden.php?taxon=4890 and
+      https://oregonflora.org/taxa/index.php?tid=13000 (the second link has a map of the locations)
+Vegitation communities meta data - https://inr.oregonstate.edu/our-products/maps#VHM
+Crop data: https://nassgeodata.gmu.edu/CropScape/
+
+  c) Purpose of the study
+I will refine my data by combining the vegitation meta data and the location of the six specific flowers that the yellow faced bombus preferes.
+I will also combine the colony count of the yellow bombus with the average temperatures of each season. This will be similar to the the map 
+I made in the lab 5 homework. 
+I will also plot which flower is preferred by the yellow faced bombus for each month of the year.
+
+This will help communicate the scientific findings in a way the reader can understand if they have no background on the the yellow faced bombus 
+and its food supply in the varying climates. The graphics can dipict the colony size in the winter and summer, and how the changing seasons 
+changes the yellow faced bompus food preference. 
+
+  d) Hypotheses/questions
+
+It is questioned how the bee maintains the colony under varying weather conditions/season. It is hypothesized that 
+the yellow faced bombus food supply will be inpacted in the winter seasons and how the diet changes as the vegitation 
+of the landscape scape changes. I predict that the yellow faced bombus colony size will decrease as the weather decreases. I also predict 
+that the food chain will have a paralleling relationship with the changing seasons, meaning, as winter appraches the vegitation differs and
+yellow faced bompus prefered food/vegitation decreases. The whole goal is to see the differences in interaction between the bombus and food supply as the weather changes, also the difference in colony size 
+as the weather and food supply changes. I am curious about the changes in weather and how it influcenes the bombus food chain and colony size. 
+
+2. Dataset Identification
+colony size is in a CSV file colony_size_database_2015_2018.csv - it was collected by James P Strange
+
+For the temperature there is an Oregon climate data website that has data of different averages of weather and parcipitation in the state of Oregon. 
+Climate data: https://prism.oregonstate.edu/
+  
+  To collect data for the yellow faced bombus vegitation througout the different seasons.
+https://www.oregon.gov/oda/shared/Documents/Publications/PlantConservation/LupinusOreganusProfile.pdf
+Blue Hydrangea - 
+  https://landscapeplants.oregonstate.edu/plants/hydrangea-macrophylla
+Eriogonum (june- september, october in higher elevations)       
+https://extension.oregonstate.edu/gardening/pollinators/eriogonum-umbellatum-sulphur-flower-buckwheat
+Phacelia (april - july) -
+  https://blogs.oregonstate.edu/gardenecologylab/tag/phacelia/
+  Clarkia (fall and spring) - https://blogs.oregonstate.edu/gardenecologylab/tag/clarkia-amoena/
+  Ericameria (june - november) https://oregonflora.org/taxa/garden.php?taxon=4890 and
+https://oregonflora.org/taxa/index.php?tid=13000 (the second link has a map of the locations)
+Vegitation communities meta data - https://inr.oregonstate.edu/our-products/maps#VHM
+
+Crop data: https://nassgeodata.gmu.edu/CropScape/
+  
+3. Workflow Plan
+1) Remove all the other coloneis of bees from the colony count 
+2) Map out the weather averages in the state of california as well as overlay the identification spots of the yellow faced bombus on top
+3) Overlay the weather averages of each season on the state of oregon and the amount of vegitation that appears for each flower the bompus prefers
+4) Overlay the colony count of the bombus throughout the climate changes in california. 
+
+My stratagy to exicute this will be similar to the lab project 5 and also I am hoping to make a food web network like displayed in lab project 
+6. 
+
+
+Resouces:
+Daane, Emily, and April 26. “Creature Feature: Yellow-Faced Bumble Bee.” Richardson Bay Audubon Center, 28 Apr. 2023, richardsonbay.audubon.org/news/creature-feature-yellow-faced-bumble-bee. 
+
+Erica Sarro, Amber Tripodi, S Hollis Woodard, Bumble Bee (Bombus vosnesenskii) Queen Nest Searching Occurs Independent of Ovary Developmental Status, Integrative Organismal Biology, Volume 4, Issue 1, 2022, obac007, https://doi.org/10.1093/iob/obac007
+
+https://explorer.natureserve.org/Taxon/ELEMENT_GLOBAL.2.819691/Bombus_vosnesenskii
+
+James P Strange, Amber D Tripodi, Thuy-Tien T Lindsay, James D Herndon, Joyce Knoblett, Morgan E Christman,
+  N Pinar Barkan, Jonathan B U Koch, Variation in North American bumble bee nest success and colony sizes under captive rearing conditions, 
+  Journal of Insect Science, Volume 23, Issue 3, May 2023, 10, https://doi.org/10.1093/jisesa/iead032
+
+
+
+
+
+
+
